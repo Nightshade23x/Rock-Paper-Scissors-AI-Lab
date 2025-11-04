c557710f964467bb9f155056e9c63d1e81af0cd0
Nightshade23x <samarmahajan23@gmail.com>
2025-11-04 16:19:18 +0200

    wrote basic code behind markov chains and added it to main game area

--- RPS Game_Code.py
+++ RPS Game_Code.py
@@ -1,8 +1,11 @@
-import random
+from AI_Code import RPS_AI
+
 def game():
+    ai=RPS_AI()
     choices=["r","p","s"]
-    player=input("Enter your choice(r,p or s)")
-    computer=random.choice(choices)
+    while True:
+        player=input("Enter your choice(r,p or s)")
+        computer=ai.choose_ai_move()
     print(f"The computer picked {computer}")
     if player==computer:
         print("It is a draw,duel again!") 
@@ -12,6 +15,7 @@ def game():
         print("You beat the system! Congrats")
     else:
         print("You lose,the computer wins!")
+    ai.store_moves(player)
 if __name__=="__main__":
     game()
     
